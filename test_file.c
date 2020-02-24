eb5841575bd66a3a872d03cc55e58068157cdfdd
ayrus-surya <suryaravi.15@gmail.com>
2020-02-24 20:41:33 +0530

    adding test files

---
+++ test_file.c
@@ -0,0 +1,12 @@
+int main()
+{
+	int x;
+	int y;
+	{
+		int z;
+		{
+	int r;
+	}
+	}
+	for(;;;)
+}
---
+++ test_file2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	//hello
+	int y;
+	int lol3;
+	for(int z=1; z<5; z++)
+	{
+		int zx = 5;
+		if(zx<x)
+		{
+			zx=10;
+			while(zx<=10)
+			{
+				zx++;
+				int r;
+				{
+					int as;
+					printf("abcd",xyz,asbcg);
+				}
+				
+}			printf("abcd",xyz,asbcg);
+		}
+		int lol2;
+		/*this 
+		is 
+		a
+		multi-line comment*/
+	}
+	lol3 =3;
+	int lol4
+}
